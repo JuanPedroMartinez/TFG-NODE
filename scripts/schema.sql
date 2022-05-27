6a1317e4c0285af2256200f7f4c48c2b5bf60519
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-05-27 20:01:43 +0200

    x

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -28,3 +28,5 @@ CREATE TABLE IF NOT EXISTS repositorios(
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%'      WITH GRANT OPTION;
